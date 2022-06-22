19824ebcd807b043ca2833aaee2c637b79c31dd1
CptDads <tjj.jonker@gmail.com>
2022-06-22 16:06:27 +0200

    Maze code clean up and progression

--- Assets/Scripts/Maze_Generator.cs
+++ Assets/Scripts/Maze_Generator.cs
@@ -1,37 +1,63 @@
 using UnityEngine;
-using Unity.Burst;
 using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Collections;
+using Random = Unity.Mathematics.Random;
 
 public class Maze_Generator : MonoBehaviour
 {
-    [SerializeField] private int2 mazeSize;
+    [SerializeField] private Vector2Int mazeSize;
     [SerializeField] private float cellSize;
 
     private void Start()
     {
-        GenerateMaze generateMaze = new GenerateMaze()
-        {
-            mazeCellSize = cellSize,
-            mazeSize = mazeSize,
-        };
-        generateMaze.Run();
+        MazeCell[] mazeCells = GenerateMaze(mazeSize, cellSize);
+        DrawMaze(mazeCells);
     }
 
-    public struct GenerateMaze : IJob
+    public MazeCell[] GenerateMaze(Vector2Int mazeSize, float cellSize, int randomSeed = -1)
     {
-        public int2 startPosiiton;
-        public int2 endPosition;
+        Random random = randomSeed == -1 ? new Random() : new Random((uint)randomSeed);
 
-        public float mazeCellSize;
+        NativeArray<MazeCell> cells = new NativeArray<MazeCell>(mazeSize.x * mazeSize.y, Allocator.TempJob);
+
+        GenerateMazeJob generateMaze = new GenerateMazeJob()
+        {
+            cells = cells,
+            mazeSize = new int2(mazeSize.x, mazeSize.y),
+            random = random
+        };
+
+        generateMaze.Run();
+
+        MazeCell[] mazeCells = generateMaze.cells.ToArray();
+
+        // Dispose Native Arrays
+        cells.Dispose();
+
+        return mazeCells;
+    }
+
+    public void DrawMaze(MazeCell[] mazeCells)
+    {
+        foreach (var cell in mazeCells)
+        {
+            if (cell.wallTop) Debug.DrawLine(GetWorldPosition(cell.x, cell.y + 1), GetWorldPosition(cell.x + 1, cell.y + 1), Color.white, 100);
+            if (cell.wallLeft) Debug.DrawLine(GetWorldPosition(cell.x, cell.y), GetWorldPosition(cell.x, cell.y + 1), Color.white, 100);
+            if (cell.wallBottom) Debug.DrawLine(GetWorldPosition(cell.x, cell.y), GetWorldPosition(cell.x + 1, cell.y), Color.white, 100);
+            if (cell.wallRight) Debug.DrawLine(GetWorldPosition(cell.x + 1, cell.y), GetWorldPosition(cell.x + 1, cell.y + 1), Color.white, 100);
+        }
+    }
+
+    public struct GenerateMazeJob : IJob
+    {
         public int2 mazeSize;
+        public NativeArray<MazeCell> cells;
+        public Random random;
 
 
         public void Execute()
         {
-            NativeArray<MazeCell> mazeCellArray = new NativeArray<MazeCell>(mazeSize.x * mazeSize.y, Allocator.Temp);
-
             for(int x = 0; x < mazeSize.x; x++)
             {
                 for(int y = 0; y < mazeSize.y; y++)
@@ -41,45 +67,73 @@ public class Maze_Generator : MonoBehaviour
                     cell.x = x;
                     cell.y = y;
                     cell.index = CalculateIndex(x, y);
-                    cell.cellSize = mazeCellSize;
 
-                    cell.wallTop = true;
-                    cell.wallLeft = true;
+                    cell.wallTop    = true;
+                    cell.wallLeft   = true;
                     cell.wallBottom = true;
-                    cell.wallRight = true;
+                    cell.wallRight  = true;
 
-                    mazeCellArray[cell.index] = cell;
-                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100);
-                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100);
+                    cell.isVisited  = false;
+
+                    cells[cell.index] = cell;
                 }
-                Debug.DrawLine(GetWorldPosition(0, mazeSize.y), GetWorldPosition(mazeSize.x, mazeSize.y), Color.white, 100);
-                Debug.DrawLine(GetWorldPosition(mazeSize.x, 0), GetWorldPosition(mazeSize.x, mazeSize.y), Color.white, 100);
             }
 
-            // Disposing the NativeArrays
-            mazeCellArray.Dispose();
+            int currentCellIndex = 0;
+
         }
 
+        private int CheckNeighbours(int currentCellIndex)
+        {
+            MazeCell currentCell = cells[currentCellIndex];
+
+            NativeArray<int> neighbourIndex = new NativeArray<int>(4, Allocator.Temp);
+            neighbourIndex[0]   = IsNeighbourUsable(CalculateIndex(currentCell.x, currentCell.y + 1));
+            neighbourIndex[1]   = IsNeighbourUsable(CalculateIndex(currentCell.x - 1, currentCell.y));
+            neighbourIndex[2]   = IsNeighbourUsable(CalculateIndex(currentCell.x, currentCell.y - 1));
+            neighbourIndex[3]   = IsNeighbourUsable(CalculateIndex(currentCell.x + 1, currentCell.y));
+
+            NativeList<int> usableNeightbourIndexes = new NativeList<int>(Allocator.Temp);
+            foreach (int cellIndex in neighbourIndex)
+                if (cellIndex != -1) usableNeightbourIndexes.Add(cellIndex);
+
+            // Dispose NativeArrays
+            neighbourIndex.Dispose();
+
+            if (usableNeightbourIndexes.Length > 0)
+                return usableNeightbourIndexes[random.NextInt(usableNeightbourIndexes.Length)];
+            return -1;            
+        }
+
+        private int IsNeighbourUsable(int index)
+            => index > -1 && !cells[index].isVisited ? index : -1;
+
         public int CalculateIndex(int x, int y)
-            => x + y * mazeSize.x;
-
-        public Vector2 GetWorldPosition(int gridPositionX, int gridPositionY)
-            => new Vector2(gridPositionX, gridPositionY) * mazeCellSize + CalculateOrigin();
-
-        public Vector2 CalculateOrigin()
-            => new Vector2(mazeSize.x, mazeSize.y) / 2f * mazeCellSize;
+        {
+            if (x < 0 || y < 0 || x > mazeSize.x || y > mazeSize.y) 
+                return -1;
+            return x + y * mazeSize.x;
+        }
     }
 
+    #region Helper functions
+    public Vector2 GetWorldPosition(int gridPositionX, int gridPositionY)
+    => new Vector2(-gridPositionX, -gridPositionY) * cellSize + CalculateOrigin();
+
+    public Vector2 CalculateOrigin()
+        => new Vector2(mazeSize.x, mazeSize.y) / 2f * cellSize;
+    #endregion
 
     public struct MazeCell
     {
         public int x, y;
         public int index;
-        public float cellSize;
 
         public bool wallTop;
         public bool wallLeft;
         public bool wallBottom;
         public bool wallRight;
+
+        public bool isVisited;
     }
 }

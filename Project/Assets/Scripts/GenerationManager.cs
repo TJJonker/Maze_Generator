2f91f0d6204c779b04a02ae7f71644d39c57b601
CptDads <tjj.jonker@gmail.com>
2022-06-24 11:02:08 +0200

    Mesh generation patch
    Mesh generation speed is now outstanding, need to find a solution for the mesh vertices and triangles cap

--- Project/Assets/Scripts/GenerationManager.cs
+++ Project/Assets/Scripts/GenerationManager.cs
@@ -13,11 +13,13 @@ public class GenerationManager : MonoBehaviour
 
     private Mesh mesh;
 
+    
     private void Awake()
     {
         mesh = new Mesh();
         meshFilter.mesh = mesh;
     }
+    
 
     private void Start() 
         => mazeGenerator = GetComponent<Maze_Generator>();
@@ -29,23 +31,30 @@ public class GenerationManager : MonoBehaviour
         mazeSize = new Vector2Int(width, height);        
 
         MazeCell[] mazeCells = mazeGenerator.GenerateMaze(mazeSize, CELL_SIZE, randomSeed);
+        var startTime = Time.realtimeSinceStartup;
         DrawMaze(mazeCells);
+        Debug.Log("Time to draw maze: " + (Time.realtimeSinceStartup - startTime));
     }
 
     public void DrawMaze(MazeCell[] mazeCells)
     {
-        mesh = new Mesh();
         MazeCell[] WallCleanedMazeCells = WallCleanUp(mazeCells);
-        foreach(var cell in WallCleanedMazeCells)
+        MeshUtils.CreateEmptyMeshArrays(100000, out Vector3[] vertices, out Vector2[] uvs, out int[] triangles);
+
+        //foreach(var cell in WallCleanedMazeCells)
+        for (int i = 0; i < WallCleanedMazeCells.Length; i++)
         {
-            if (cell.wallTop)   CreateWall(GetWorldPosition(cell.x, cell.y + 1), GetWorldPosition(cell.x + 1, cell.y + 1));
-            if (cell.wallLeft)  CreateWall(GetWorldPosition(cell.x, cell.y), GetWorldPosition(cell.x, cell.y + 1));
-            if (cell.wallBottom)CreateWall(GetWorldPosition(cell.x, cell.y), GetWorldPosition(cell.x + 1, cell.y));
-            if (cell.wallRight) CreateWall(GetWorldPosition(cell.x + 1, cell.y), GetWorldPosition(cell.x + 1, cell.y + 1));
+            var cell = WallCleanedMazeCells[i];
+            int index = i * 4;
+
+            if (cell.wallTop)   CreateWall(GetWorldPosition(cell.x, cell.y + 1), GetWorldPosition(cell.x + 1, cell.y + 1), index, ref vertices, ref uvs, ref triangles);
+            if (cell.wallLeft)  CreateWall(GetWorldPosition(cell.x, cell.y), GetWorldPosition(cell.x, cell.y + 1), index + 1, ref vertices, ref uvs, ref triangles);
+            if (cell.wallBottom)CreateWall(GetWorldPosition(cell.x, cell.y), GetWorldPosition(cell.x + 1, cell.y), index + 2, ref vertices, ref uvs, ref triangles);
+            if (cell.wallRight) CreateWall(GetWorldPosition(cell.x + 1, cell.y), GetWorldPosition(cell.x + 1, cell.y + 1), index + 3, ref vertices, ref uvs, ref triangles);
             //break;
         }
 
-        mesh.RecalculateNormals();
+        MeshUtils.ApplyToMesh(mesh, vertices, uvs, triangles);
         meshFilter.mesh = mesh;
 
         
@@ -55,7 +64,7 @@ public class GenerationManager : MonoBehaviour
             if (cell.wallLeft)  Debug.DrawLine(GetWorldPosition(cell.x, cell.y), GetWorldPosition(cell.x, cell.y + 1), Color.black, 100);
             if (cell.wallBottom)Debug.DrawLine(GetWorldPosition(cell.x, cell.y), GetWorldPosition(cell.x + 1, cell.y), Color.black, 100);
             if (cell.wallRight) Debug.DrawLine(GetWorldPosition(cell.x + 1, cell.y), GetWorldPosition(cell.x + 1, cell.y + 1), Color.black, 100);
-            break;   
+            //break;   
         }
         
     }
@@ -63,31 +72,15 @@ public class GenerationManager : MonoBehaviour
 
 
     #region Helper functions
-    private void CreateWall(Vector2 pointA, Vector2 pointB)
+    private void CreateWall(Vector2 pointA, Vector2 pointB, int index, ref Vector3[] vertices, ref Vector2[] uvs, ref int[] triangles)
     {
         var shapeDirection = pointB - pointA;
 
         pointA += new Vector2(.025f, .025f);
         pointB -= new Vector2(.025f, .025f);
-        /*
-        if(shapeDirection.x == 0)
-        {
-            pointA.x += .025f;
-            pointB.x -= .025f;
-        }
-        if(shapeDirection.y == 0)
-        {
-            pointA.y += .025f;
-            pointB.y -= .025f;
-        }
-        */
-        MeshUtils.AddToMesh(ref mesh, pointA + shapeDirection * .5f, 0f, pointB - pointA, Vector2.zero, Vector2.one);
 
-
-        //var shapeDirection = pointB - pointA;
-        //if (shapeDirection.x > 0) pointA.y -= .5f;
-        //if (shapeDirection.y > 0) pointA.x -= .5f;
-        //MeshUtils.AddToMesh(ref mesh, pointA, 0f, pointB - pointA, Vector2.zero, Vector2.zero);
+        MeshUtils.AddToMeshArray(ref vertices, ref uvs, ref triangles, index, pointA + shapeDirection * .5f, 0f, pointB - pointA, Vector2.zero, Vector2.one);
+        //MeshUtils.AddToMesh(ref mesh, pointA + shapeDirection * .5f, 0f, pointB - pointA, Vector2.zero, Vector2.one);
     }
 
     private MazeCell[] WallCleanUp(MazeCell[] mazeCells)
